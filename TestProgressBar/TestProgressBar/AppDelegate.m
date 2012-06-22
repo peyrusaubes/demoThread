0a18cf2f3cbadfc05ab35a720159f06ded84aa21
Denis Peyrusaubes <denispeyrusaubes@macbook-pro-de-denis-peyrusaubes.local>
2012-06-22 10:09:11 +0200

    Ajout d'un NSLog au démarage de l'applcation

--- TestProgressBar/TestProgressBar/AppDelegate.m
+++ TestProgressBar/TestProgressBar/AppDelegate.m
@@ -14,6 +14,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+	NSLog(@"Démarage !!");
     // Override point for customization after application launch.
     return YES;
 }
